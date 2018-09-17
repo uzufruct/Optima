cb3a99ad87af2f758c63b202ad97786d8e0ca134
Nikita.Sidorov <nickorussisch@gmail.com>
2018-09-17 19:21:35 +0700

    Implemented a new class Stat

--- lab1/stat.rb
+++ lab1/stat.rb
@@ -1,5 +1,40 @@
-require "csv"
+require 'csv'
 
-data = CSV.read(data.csv)
+class Stat
 
-puts data
+  def initialize(file, line)
+    @data = CSV.read(file)[line - 1].collect {|i| i.to_f}
+  end
+
+  def compute
+    @mean = (@data.max + @data.min) / 2.0
+    puts 'Which statistical parameter would you like to compute?
+      1 - max value
+      2 - min value
+      3 - mean value
+      4 - sample variance'
+    puts '->'
+    choice = gets.chomp
+    case choice
+    when '1'
+      @data.max
+    when '2'
+      @data.min
+    when '3'
+      @mean
+    when '4'
+      sum = 0.0
+      for i in @data
+        sum += (i - @mean) * 2
+      end
+      sum / (@data.length - 1)
+    end
+
+
+    end
+
+end
+
+path = 'C:\Users\Nikita.Sidorov\Study\Optima\lab1\data.csv'
+obj = Stat.new(path, 2)
+puts obj.compute
